4d6b94fddb1ace0127d7d1deb1ff194727902081
holoscopecheck <holoscopecheck@gmail.com>
2025-09-07 17:49:15 +0900

    シードデータの作成とDevicesモデルの作成

---
+++ prisma/seed/Users.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+import * as argon2 from "argon2";
+
+export const users = async (prisma: PrismaClient) => {
+  console.log("Usersテーブルにseedデータ挿入中...");
+
+  await prisma.users.deleteMany();
+
+  const hashOptions = {
+    type: argon2.argon2id,
+    memoryCost: 2 ** 16,
+    timeCost: 3,
+    parallelism: 1,
+  };
+
+  const createUsers = [];
+  for (let i = 1; i <= 10; i++) {
+    const username = `admin${i}`;
+    const password = `password${i}`;
+
+    const hashedPassword = argon2.hash(password, hashOptions);
+
+    createUsers.push(
+      prisma.users.create({
+        data: {
+          name: username,
+          password_hash: hashedPassword,
+        },
+      })
+    );
+  }
+
+  await Promise.all(createUsers);
+};
